dd7b2822dc138a3c95e77f3c6b6f9f62c69b5630
Lew Lefton <lew.lefton@gatech.edu>
2024-10-23 21:31:32 -0400

    Added Technology Page

--- frontend/src/pages/Technology.tsx
+++ frontend/src/pages/Technology.tsx
@@ -26,8 +26,7 @@ const Technology = () => {
             lineHeight='24px'
             color='#FFFFFF'
             fontSize={{base: '12px', lg: '16px'}}
-          >
-            Like other Theme 1 projects which are part of the{' '}
+          > Like other Theme 1 projects which are part of the{' '}
             <a
               href='https://www.proto-okn.net/'
               target='_blank'
@@ -43,10 +42,18 @@ const Technology = () => {
               rel='noreferrer'
               style={{color: 'cornsilk', textDecoration: 'underline'}}
             >
-              FRINK Team. FRINK will be hosting knowledge graphs
-              for all the Proto-OKN projects,which will help with
-              data integration and linkage across knowledge graphs.
-              
+              FRINK Team
+            </a>{'. '}
+            FRINK will be hosting knowledge graphs for all the Proto-OKN projects,
+            which will help with data integration and linkage across
+            knowledge graphs.
+          </Text>
+          <Text
+            mt='.45rem'
+            lineHeight='24px'
+            color='#FFFFFF'
+            fontSize={{ base: '12px', lg: '16px' }}
+          >
               Currently, our data requires both RDF and RDF-star
               triples. FRINK is not yet able to support the RDF-star
               specification, so the alpha version of our web
@@ -59,14 +66,81 @@ const Technology = () => {
               graph databases and relational databases (and potentially
               NoSQL databases, too) for flexibility, schema integration,
               and to be able to optimize performance.
-
+          </Text>
+          <Text
+            mt='.45rem'
+            lineHeight='24px'
+            color='#FFFFFF'
+            fontSize={{ base: '12px', lg: '16px' }}
+          >
               The web application for CollabNext is currently
               designedwith a Flask/Python backend and a
               React/Typescript/Javascript frontend.
-              These are both hosted on Microsoft Azure, and funded with an allocation to the NSF’s Cloudbankservice.We have developed APIs which connect to our data sources and provide a standardized approach for our web application frontend to display filtered datasets.Our alpha deliverable is hosted on Github, and we also use Dropbox for shared storage, Trello for task management, Slack for our team communication. Two of our challenges are Topic Classification and Name disambiguation. We are currently using OpenAlex subfields as topic classifiers, but it is becoming increasingly clear that we will need to build a robust Machine Learning model to help bridge human understandable research topics and semantic web ontologies and data schemas which may categorize research topics in a more machine-friendly way.Name disambiguationwill always be a challenge for us since we want to maintain a person-focused approach. In addition to the built-in algorithms used by OpenAlex, we are developing internal models which use more current AI and ML research, and which we believe will yield animprovement
-              for all our entity resolution challenges.
+              These are both hosted on Microsoft Azure, and
+            funded with an allocation to the
+            <a
+              href='https://www.cloudbank.org/'
+              target='_blank'
+              rel='noreferrer'
+              style={{ color: 'cornsilk', textDecoration: 'underline' }}
+            >
+              NSF’s Cloudbank service.
+            </a>{' '}
+          </Text>
+          <Text
+            mt='.45rem'
+            lineHeight='24px'
+            color='#FFFFFF'
+            fontSize={{ base: '12px', lg: '16px' }}
+          > We have developed APIs which connect to our data sources and provide a
+standardized approach for our web application frontend to display filtered
+datasets.
+          </Text>
+          <Text
+            mt='.45rem'
+            lineHeight='24px'
+            color='#FFFFFF'
+            fontSize={{ base: '12px', lg: '16px' }}
+          >
+            Our alpha deliverable is{' '}
+            <a
+              href='https://github.com/OKN-CollabNext/CollabNext_public
+'
+              target='_blank'
+              rel='noreferrer'
+              style={{ color: 'cornsilk', textDecoration: 'underline' }}
+            >
+              hosted on Github
+            </a>{', '}
+            and we also use Dropbox for shared storage,
+            Trello for task management, and
+            Slack for our team communication.
+          </Text>
+          <Text
+            mt='.45rem'
+            lineHeight='24px'
+            color='#FFFFFF'
+            fontSize={{ base: '12px', lg: '16px' }}
+          >
+Two of our challenges are Topic Classification and Name disambiguation. We are
+currently using OpenAlex subfields as topic classifiers, but it is becoming
+increasingly clear that we will need to build a robust Machine Learning model to
+help bridge human understandable research topics and semantic web ontologies and
+data schemas which may categorize research topics in a more machine-friendly
+way.
+          </Text>
+          <Text
+            mt='.45rem'
+            lineHeight='24px'
+            color='#FFFFFF'
+            fontSize={{ base: '12px', lg: '16px' }}
+          >
+Name disambiguationwill always be a challenge for us since we want to
+maintain a person-focused approach. In addition to the built-in algorithms used
+by OpenAlex, we are developing internal models which use more current AI and ML
+research, and which we believe will yield animprovement for all our entity
+resolution challenges.
 
-            </a>
           </Text>
         </Box>
       </Box>
