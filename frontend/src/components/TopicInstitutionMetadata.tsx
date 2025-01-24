d87748843e350d9e05608b6c32de04fe17399193
Nikhil Aryal <aryaln0058@gmail.com>
2025-01-24 15:15:29 -0600

    Refactored topic clusters

--- frontend/src/components/TopicInstitutionMetadata.tsx
+++ frontend/src/components/TopicInstitutionMetadata.tsx
@@ -1,17 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { Box, Flex, Text } from '@chakra-ui/react';
 
 import { ResearchDataInterface } from '../utils/interfaces';
+import TopicClusterGraphComponent from './TopicClusterGraphComponent';
 
 const TopicInstitutionMetadata = ({data}: {data: ResearchDataInterface}) => {
+  const [showTopicClusterGraph, setTopicClusterGraph] = useState(false);
+  const handleTopicClusterClick = () => {
+    setTopicClusterGraph(!showTopicClusterGraph)
+  }
+
+  const transformTopicClustersToGraph = (topicClusters: string[]) => {
+    const nodes: { id: string; label: string; type: string; }[] = [];
+
+    const topicName = `topic_${data?.topic_name}`;
+    nodes.push({
+      id: topicName,
+      label: data.topic_name,
+      type: 'TOPIC'
+    });
+
+    topicClusters.forEach((topic, id) => {
+      const subfieldNodeId = `subfield_${id}`;
+      nodes.push({
+        id: subfieldNodeId,
+        label: topic,
+        type: 'SUBFIELD',
+      });
+      
+    });
+    return {
+      nodes,
+    };
+  };
+
   return (
-    <Flex
-      display={{base: 'block', lg: 'flex'}}
+      <Flex
+      display={{ base: 'block', lg:'flex'}}
       justifyContent={'space-between'}
       mt='0.6rem'
       className='list-map'
-    >
+      >
       <Box w={{lg: '34%'}}>
         <button className='topButton'>List Map</button>
         <h2>{data?.institution_name}</h2>
@@ -32,41 +62,45 @@ const TopicInstitutionMetadata = ({data}: {data: ResearchDataInterface}) => {
           RORID -{' '}
           {data?.ror_link?.split('/')[data?.ror_link?.split('/')?.length - 1]}
         </a>
+
         <Box mt='0.4rem'>
           <Text fontSize={'17px'} fontWeight={'600'}>
-            Topic Clusters
-          </Text>
-          <Box ml='2rem'>
-            {data?.topic_clusters?.map((cluster) => (
-              <Text mt='0.3rem'>{cluster}</Text>
+            <a style={{ cursor: 'pointer', 'textDecoration': 'underline' }} onClick={handleTopicClusterClick}>
+              Topic Clusters
+            </a>
+          </Text> 
+        </Box>
+      </Box>
+
+      {showTopicClusterGraph ? (
+        <Box mt='1rem' w={{lg: '70%'}} mx = 'auto'>
+            <TopicClusterGraphComponent graphData={transformTopicClustersToGraph(data?.topic_clusters)} />
+          </Box>) 
+        : (
+          <Box w={{lg: '64%'}} mt={{base: '.9rem', lg: 0}}>
+          <Box display={'flex'} justifyContent={'space-between'}>
+            <Text fontSize={'18px'} fontWeight={600} w='72%'>
+              Person
+            </Text>
+            <Text fontSize={'18px'} fontWeight={600} w='26%'>
+              No of works
+            </Text>
+          </Box>
+          <Box mt='.5rem'>
+            {data?.authors?.map((topic) => (
+              <Flex justifyContent={'space-between'}>
+                <Text fontSize='14px' w='72%'>
+                  {topic[0]}
+                </Text>
+                <Text fontSize='14px' w='26%'>
+                  {topic[1]}
+                </Text>
+              </Flex>
             ))}
           </Box>
         </Box>
-      </Box>
-      <Box w={{lg: '64%'}} mt={{base: '.9rem', lg: 0}}>
-        <Box display={'flex'} justifyContent={'space-between'}>
-          <Text fontSize={'18px'} fontWeight={600} w='72%'>
-            Person
-          </Text>
-          <Text fontSize={'18px'} fontWeight={600} w='26%'>
-            No of works
-          </Text>
-        </Box>
-        <Box mt='.5rem'>
-          {data?.authors?.map((topic) => (
-            <Flex justifyContent={'space-between'}>
-              <Text fontSize='14px' w='72%'>
-                {topic[0]}
-              </Text>
-              <Text fontSize='14px' w='26%'>
-                {topic[1]}
-              </Text>
-            </Flex>
-          ))}
-        </Box>
-      </Box>
+      )}
     </Flex>
-  );
-};
+  )}
 
 export default TopicInstitutionMetadata;
